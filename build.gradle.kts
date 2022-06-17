f0d397a4a093f5bb62cb81cfbd5ef7ef74f958ae
Eivind Siqveland Larsen <eivind@siqve.land>
2022-06-18 01:09:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
 import com.google.protobuf.gradle.*
 
-val ktorVersion = "1.6.6"
-val logstashEncoderVersion = "7.0.1"
-val kotlinVersion = "1.3.40"
+val ktorVersion = "2.0.2"
+val logstashEncoderVersion = "7.2"
 val junitJupiterVersion = "5.8.2"
 val protoGradlePluginVersion = "0.8.18"
-val protocVersion = "3.19.1"
-val grpcVersion = "1.42.1"
-val grpcKotlinVersion = "1.2.0"
-val tikaVersion = "2.1.0"
+val protocVersion = "3.21.1"
+val grpcVersion = "1.47.0"
+val grpcKotlinVersion = "1.3.0"
+val tikaVersion = "2.3.0"
 
 group = "com.github.esiqveland"
 version = "1.0"
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    id("com.parmet.buf") version "0.3.1"
+    kotlin("jvm") version "1.7.0"
+    id("com.parmet.buf") version "0.5.1"
     id("com.google.protobuf") version "0.8.18"
     id("idea")
 }
@@ -42,12 +41,13 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
-    implementation("io.ktor:ktor-jackson:$ktorVersion")
-    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("io.vavr:vavr:1.0.0-alpha-4")
-    implementation("org.rocksdb:rocksdbjni:6.26.1")
+    implementation("org.rocksdb:rocksdbjni:7.2.2")
 
-    implementation("io.reactivex.rxjava3:rxjava:3.1.3")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
 
     implementation("org.apache.tika:tika-parsers-standard-package:$tikaVersion")
     implementation("org.apache.tika:tika-parsers:$tikaVersion")
@@ -64,22 +64,22 @@ dependencies {
     implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
     compileOnly("org.apache.tomcat:annotations-api:6.0.53") // necessary for Java 9+
 
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
 
     implementation("ch.qos.logback:logback-classic:1.3.0-alpha10")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("io.mockk:mockk:1.12.0")
-    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "junit")
     }
 }
 
 java {
-    sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
+    sourceCompatibility = org.gradle.api.JavaVersion.VERSION_19
 }
 
 tasks {
@@ -91,10 +91,10 @@ tasks {
         jvmArgs("--enable-preview")
     }
     compileKotlin {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "19"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "19"
     }
 
     named<Jar>("jar") {
